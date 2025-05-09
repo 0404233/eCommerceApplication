d9bb6ae88c21a927c6a4e9c0430456c7187066ca
0404233 <andrey0404233@gmail.com>
2025-05-09 18:25:18 +0300

    feat: set header to top of page

---
+++ src/components/layout/Footer/Footer.tsx
@@ -0,0 +1,6 @@
+import { ReactElement } from 'react';
+import styles from './Footer.module.css';
+
+export default function Footer(): ReactElement {
+  return <footer className={styles['footer']}>Footer</footer>;
+}
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,5 +1,12 @@
 import Header from '../../components/layout/Header/Header';
+import Footer from '../../components/layout/Footer/Footer';
+import styles from './MainPage.module.css';
 
 export default function MainPage() {
-  return <Header />;
+  return (
+    <main className={styles['main']}>
+      <Header></Header>
+      <Footer></Footer>
+    </main>
+  );
 }
