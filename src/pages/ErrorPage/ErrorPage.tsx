40052dabb9934169d85e709f5af4e62b1ca0e82e
Alexander Iluishenko <126574246+sashaSVNT@users.noreply.github.com>
2025-05-09 15:56:17 +0300

    refactor: update url name in ErrorPage.tsx

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -2,13 +2,13 @@ import classes from './ErrorPage.module.css'
 import { useNavigate } from 'react-router';
 
 export default function ErrorPage() {
-  const incorrectUrl = window.location.pathname.slice(1);
+  const url = window.location.pathname.slice(1);
   
   const navigate = useNavigate();
 
   return (
     <div className={classes["error-page__wrapper"]}>
-      <h1 className={classes["error-page__title"]}>Page <span className={classes['incorrect-url']}>"{incorrectUrl}"</span> is not found</h1>
+      <h1 className={classes["error-page__title"]}>Page <span className={classes['incorrect-url']}>"{url}"</span> is not found</h1>
       <button 
       className={classes['error__btn']}
       onClick={() => navigate('/')}
