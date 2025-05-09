44e1b972fd10fbbe5c9645dd43598464d4d6e6d3
heyArtik-dev <heyArtik@rambler.ru>
2025-05-09 14:20:21 +0300

    feat: add pages / error page / navigation between login-register-notFound

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,11 +1,26 @@
 import { Route, Routes } from "react-router";
 import Login from "../pages/Login/Login";
 import Registration from "../pages/Registration/Registration";
+import MainPage from "../pages/Main/Main";
+import CatalogProduct from "../pages/CatalogProduct/CatalogProduct";
+import Basket from "../pages/Basket/Basket";
+import AboutUs from "../pages/AboutUs/AboutUs";
+import UserProfile from "../pages/UserProfile/UserProfile";
+import DetailedProduct from "../pages/DetailedProduct/DetailedProduct";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 export default function AppRoutes() {
   const navigationRoutes = [
+    { path: '/', element: <MainPage /> },
+    { path: '/main', element: <MainPage /> },
     { path: '/login', element: <Login /> },
-    { path: '/register', element: <Registration /> }
+    { path: '/register', element: <Registration /> },
+    { path: '/user', element: <UserProfile /> },
+    { path: '/catalog', element: <CatalogProduct /> },
+    { path: '/product', element: <DetailedProduct /> },
+    { path: '/basket', element: <Basket /> },
+    { path: '/about', element: <AboutUs /> },
+    { path: '*', element: <ErrorPage /> },
   ]
 
   return <Routes>
