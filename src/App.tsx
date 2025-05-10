1f6594eee16cc1b56afb664e9915fa4d113422ed
0404233 <andrey0404233@gmail.com>
2025-05-10 14:15:31 +0300

    feat: implement registration via commercetools SDK

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import { ReactElement } from 'react';
+import {
+  ReactElement,
+  useEffect,
+} from 'react';
 import './App.css';
 import AppRoutes from './routes/Routes';
 
-// const AUTH_URL = import.meta.env['VITE_AUTH_URL'];
-// const CLIENT_SECRET = import.meta.env['VITE_CLIENT_SECRET'];
-// const CLIENT_ID = import.meta.env['VITE_CLIENT_ID'];
-// const SCOPES = import.meta.env['VITE_SCOPES'];
-
-// type AuthResponse = {
-//   access_token: string;
-//   expires_in: number;
-//   scope: string;
-//   token_type: string;
-// };
-
 function App(): ReactElement {
-  // const [login, setLogin] = useState('');
-  // const [password, setPassword] = useState('');
-  // const [authKey, setAuthKey] = useState('');
-  // const onSubmit = (e: FormEvent) => {
-  //   console.log(authKey);
-  //   e.preventDefault();
-  //   console.log(login, password);
-  // };
   // useEffect(() => {
   //   async function getToken(): Promise<void> {
   //     const credentials = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
@@ -39,36 +22,17 @@ function App(): ReactElement {
   //       body: params.toString(),
   //     });
   //     const data: AuthResponse = await response.json();
-  //     setAuthKey(data.access_token);
+  //     console.log(data);
   //   }
   //   getToken();
   // }, []);
-  return (
-    // <div className="wrapper">
-    //   <form onSubmit={onSubmit}>
-    //     <label htmlFor="login">Login</label>
-    //     <input
-    //       type="text"
-    //       name=""
-    //       id="login"
-    //       value={login}
-    //       onChange={(e) => setLogin(e.target.value)}
-    //     />
-    //     <label htmlFor="password">Password</label>
-    //     <input
-    //       type="text"
-    //       name=""
-    //       id="password"
-    //       value={password}
-    //       onChange={(e) => setPassword(e.target.value)}
-    //     />
-    //     <button type="submit">submit</button>
-    //   </form>
-    // </div>
+  useEffect(() => {
+    const controller =
+      new AbortController();
 
-    <AppRoutes />
-
-  );
+    return () => controller.abort();
+  }, []);
+  return <AppRoutes />;
 }
 
 export default App;
---
+++ src/BuildClient.ts
@@ -0,0 +1,60 @@
+import {
+  ClientBuilder,
+  type AuthMiddlewareOptions, // Required for auth
+  type HttpMiddlewareOptions, // Required for sending HTTP requests
+} from '@commercetools/ts-client';
+
+const AUTH_URL = import.meta.env[
+  'VITE_AUTH_URL'
+];
+const API_URL = import.meta.env[
+  'VITE_API_URL'
+];
+const CLIENT_SECRET = import.meta.env[
+  'VITE_CLIENT_SECRET'
+];
+const CLIENT_ID = import.meta.env[
+  'VITE_CLIENT_ID'
+];
+const SCOPES = import.meta.env[
+  'VITE_SCOPES'
+];
+const PROJECT_KEY = import.meta.env[
+  'VITE_PROJECT_KEY'
+];
+
+const projectKey = `${PROJECT_KEY}`;
+const scopes = [`${SCOPES}`];
+
+// Configure authMiddlewareOptions
+const authMiddlewareOptions: AuthMiddlewareOptions =
+  {
+    host: `${AUTH_URL}`,
+    projectKey: projectKey,
+    credentials: {
+      clientId: `${CLIENT_ID}`,
+      clientSecret: `${CLIENT_SECRET}`,
+    },
+    scopes,
+    httpClient: fetch,
+  };
+
+// Configure httpMiddlewareOptions
+const httpMiddlewareOptions: HttpMiddlewareOptions =
+  {
+    host: `${API_URL}`,
+    httpClient: fetch,
+  };
+
+// Export the ClientBuilder
+export const ctpClient =
+  new ClientBuilder()
+    .withProjectKey(projectKey) // .withProjectKey() is not required if the projectKey is included in authMiddlewareOptions
+    .withClientCredentialsFlow(
+      authMiddlewareOptions,
+    )
+    .withHttpMiddleware(
+      httpMiddlewareOptions,
+    )
+    .withLoggerMiddleware() // Include middleware for logging
+    .build();
