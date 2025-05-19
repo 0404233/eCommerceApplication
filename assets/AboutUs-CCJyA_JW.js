cf6ebfa38a1c4a583b0fac9f230b7f84e7908be2
Aliaksandr Iluishenka <sasha29292931@gmail.com>
2025-05-20 01:50:50 +0300

    Updates

--- assets/AboutUs-CCJyA_JW.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-LKDfAJNA.js";function r(){return t.jsx("h1",{children:"About Us page"})}export{r as default};
--- assets/Basket-CMuA15Ng.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-LKDfAJNA.js";function r(){return t.jsx("h1",{children:"Basket page"})}export{r as default};
