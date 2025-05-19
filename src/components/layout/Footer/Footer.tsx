da79271749bd543466997e0aa604d258fc4fd315
0404233 <andrey0404233@gmail.com>
2025-05-20 00:47:50 +0300

    fix: add git sensitive to register

---
+++ src/components/layout/Footer/Footer.tsx
@@ -0,0 +1,6 @@
+import { ReactElement } from 'react';
+import styles from './footer.module.css';
+
+export default function Footer(): ReactElement {
+  return <footer className={styles['footer']}>FOOTER</footer>;
+}
