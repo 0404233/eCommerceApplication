8fc6aa78c334d9da7bde437e0d313b056e5f8b45
0404233 <andrey0404233@gmail.com>
2025-06-03 22:46:42 +0300

    Updates

---
+++ assets/Basket-CsdIRomk.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Bzmf8EPj.js";function r(){return t.jsx("h1",{children:"Basket page"})}export{r as default};
--- assets/UserProfile-B0wml69F.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-zxBxM_HK.js";function o(){return r.jsx("h1",{children:"User Profile page"})}export{o as default};
