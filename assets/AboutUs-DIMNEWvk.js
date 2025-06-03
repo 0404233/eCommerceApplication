53d47d2fce76c2638a755c5aa14e040966944d97
0404233 <andrey0404233@gmail.com>
2025-06-03 23:08:00 +0300

    Updates

---
+++ assets/AboutUs-DIMNEWvk.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BN0l0VlD.js";function r(){return t.jsx("h1",{children:"About Us page"})}export{r as default};
---
+++ assets/Basket-CvDiZ6PT.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BN0l0VlD.js";function r(){return t.jsx("h1",{children:"Basket page"})}export{r as default};
